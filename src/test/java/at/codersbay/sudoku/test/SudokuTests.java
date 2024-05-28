b87eec13c6c364b3fa890731e23c7c461e853538
Aenton <Andreas.Weber1988@gmx.at>
2024-05-28 09:34:28 +0200

    Fix structure (pull git root into correct folder)

---
+++ src/test/java/at/codersbay/sudoku/test/SudokuTests.java
@@ -0,0 +1,13 @@
+package at.codersbay.sudoku.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
